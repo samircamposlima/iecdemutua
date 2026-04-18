ec05638ac879508904352758b626f8e82b64590d
Samir <samircamposlima@gmail.com>
2026-04-17 21:37:00 -0300

    fix: build ok, bibles no apk, pendente ajuste no hermes

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,7 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 const config = {
-  resolver: {
-    assetExts: [...getDefaultConfig(__dirname).resolver.assetExts, 'sqlite', 'db'],
-  },
+  resolver: {},
 };
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
