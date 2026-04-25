2e001b22f867798440109694a2d332c0ccdb8cd0
Samir <samircamposlima@gmail.com>
2026-04-25 11:54:17 -0300

    chore(deps): instala dependências de splash screen e configura canais de notificação no App.js

--- App.js
+++ App.js
@@ -3,27 +3,30 @@ import React,{ useEffect } from 'react';
 import { AuthProvider } from './src/contexts/AuthContext';
 import RootNavigator from './src/navigation/RootNavigator';
 import notifee, { AndroidImportance } from '@notifee/react-native';
+import BootSplash from "react-native-bootsplash";
 
 export default function App() {
 
- // App.js
-useEffect(() => {
-  const setupNotifications = async () => {
-    // Cria o canal com configurações de alta prioridade
-    await notifee.createChannel({
-      id: 'eventos_lembretes',
-      name: 'Lembretes e Pedidos',
-      importance: AndroidImportance.HIGH,
-      vibration: true, // Adicionado conforme App.tsx
-    });
+  useEffect(() => {
+    const setupNotifications = async () => {
+      await notifee.createChannel({
+        id: 'eventos_lembretes',
+        name: 'Lembretes e Pedidos',
+        importance: AndroidImportance.HIGH,
+        vibration: true,
+      });
 
-    await notifee.requestPermission();
-  };
+      await notifee.requestPermission();
+      
+      // ESCONDER A SPLASH SCREEN AQUI
+      // O delay de 250ms evita um "piscar" brusco na transição
+      setTimeout(() => {
+        BootSplash.hide({ fade: true });
+      }, 250);
+    };
 
-  setupNotifications();
-}, []);
-
-
+    setupNotifications();
+  }, []);
 
   return (
     <AuthProvider>
